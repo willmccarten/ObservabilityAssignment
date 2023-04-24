b34d87419f28bd2040bb6a729eab6ef059820c31
willmccarten <william.mccarten@ufl.edu>
2023-04-24 00:31:10 -0400

    trace mods again

--- tracing.js
+++ tracing.js
@@ -4,24 +4,36 @@ const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 const { trace } = require("@opentelemetry/api");
+
 //Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
 //Exporter
 module.exports = (serviceName) => {
    const jaegerExporter = new JaegerExporter({
+    
        serviceName: serviceName,
-       endpoint: 'http://localhost:14268/api/traces', // Update with your Jaeger endpoint
+       endpoint: 'http://localhost:14268/api/traces', 
+       verbose: true,
+
    });
+
+   const span = trace.startSpan('processTodo');
+   span.setAttribute('component', 'todos');
+   
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
        }),
    });
+
    provider.addSpanProcessor(new BatchSpanProcessor(jaegerExporter));
+
    provider.register();
+
    registerInstrumentations({
        instrumentations: [
            new HttpInstrumentation(),
@@ -30,5 +42,6 @@ module.exports = (serviceName) => {
        ],
        tracerProvider: provider,
    });
+
    return trace.getTracer(serviceName);
 };
