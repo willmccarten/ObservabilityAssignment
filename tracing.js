cd17a878c03a286d6947da735f04c2f773ffa4b1
willmccarten <william.mccarten@ufl.edu>
2023-04-24 00:53:53 -0400

    tracing attempt

--- tracing.js
+++ tracing.js
@@ -1,51 +1,40 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 const { trace } = require("@opentelemetry/api");
-
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 //Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 
-//Exporter
-module.exports = (serviceName) => {
+function createTracer(serviceName) {
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+  
+  const exporter = new JaegerExporter({
+    serviceName: serviceName,
+    host: 'localhost',
+    port: 6832,
+  });
+  
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+  provider.register();
+  
+  registerInstrumentations({
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+      new MongoDBInstrumentation(),
+    ],
+    tracerProvider: provider,
+  });
 
-   const tracer = trace.getTracer(serviceName);
+  return trace.getTracer(serviceName);
+}
 
-   const jaegerExporter = new JaegerExporter({
-    
-       serviceName: serviceName,
-       endpoint: 'http://localhost:14268/api/traces', 
-       verbose: true,
-
-   });
-
-   const span = tracer.startSpan('processTodo');
-   span.setAttribute('component', 'todos');
-   
-   const provider = new NodeTracerProvider({
-       tracer: tracer,
-       resource: new Resource({
-           [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-       }),
-   });
-
-   provider.addSpanProcessor(new BatchSpanProcessor(jaegerExporter));
-
-   provider.register();
-
-   registerInstrumentations({
-       instrumentations: [
-           new HttpInstrumentation(),
-           new ExpressInstrumentation(),
-           new MongoDBInstrumentation(),
-       ],
-       tracerProvider: provider,
-   });
-
-   return trace.getTracer(serviceName);
-};
+module.exports = createTracer;
